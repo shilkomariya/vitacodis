71494107bdfd01785d3b78f2dca62d142904291a
shilkomariya <shilkomariya@gmail.com>
2021-11-14 15:19:25 +0300

    15-10

--- youzer/account-template.php
+++ youzer/account-template.php
@@ -1,88 +1,66 @@
-<?php do_action( 'youzer_account_before_content' ); ?>
+<?php do_action('youzer_account_before_content'); ?>
 
 <?php
-	$action = bp_current_action();
-	$is_avatar_page = FALSE;
-	if ($action == 'change-avatar' || $action == 'change-cover-image'){
-		$is_avatar_page = TRUE;
-	}
+$action = bp_current_action();
+$is_avatar_page = FALSE;
+if ($action == 'change-avatar' || $action == 'change-cover-image') {
+    $is_avatar_page = TRUE;
+}
 ?>
 
 <div id="youzer">
 
-	<div id="<?php echo apply_filters( 'yz_account_template_id', 'yz-bp' ); ?>" class="youzer yz-page yz-account-page">
+    <div id="<?php echo apply_filters('yz_account_template_id', 'yz-bp'); ?>" class="youzer yz-page yz-account-page">
 
-		<?php do_action( 'youzer_account_before_main' ); ?>
+	<?php do_action('youzer_account_before_main'); ?>
 
-		<main class="yz-page-main-content <?php echo $is_avatar_page ? 'avatar-pages' : 'non-avatar-pages'; ?>">
+	<main class="yz-page-main-content <?php echo $is_avatar_page ? 'avatar-pages' : 'non-avatar-pages'; ?>">
 
-			<?php
-				if (!$is_avatar_page) {
-			?>
+	    <?php
+	    if (!$is_avatar_page) {
+		?>
 
-				<aside class="youzer-sidebar yz-settings-sidebar">
+    	    <aside class="youzer-sidebar yz-settings-sidebar">
 
-					<?php do_action( 'youzer_settings_menus' ); ?>
+		    <?php do_action('youzer_settings_menus'); ?>
 
-				</aside>
+    	    </aside>
 
-			<?php } ?>
+	    <?php } ?>
 
-			<div class="youzer-main-content settings-main-content">
+	    <div class="youzer-main-content settings-main-content">
 
-				<?php do_action( 'bp_before_member_settings_template' ); ?>
+		<?php do_action('bp_before_member_settings_template'); ?>
 
-				<div id="template-notices" role="alert" aria-atomic="true">
-					<?php
+		<div id="template-notices" role="alert" aria-atomic="true">
+		    <?php
+		    /**
+		     * Fires towards the top of template pages for notice display.
+		     *
+		     * @since 1.0.0
+		     */
+		    do_action('template_notices');
+		    ?>
 
-					/**
-					 * Fires towards the top of template pages for notice display.
-					 *
-					 * @since 1.0.0
-					 */
-					do_action( 'template_notices' ); ?>
+		</div>
 
-				</div>
+		<div class="youzer-inner-content settings-inner-content">
 
-				<div class="youzer-inner-content settings-inner-content">
+		    <?php do_action('youzer_account_before_form'); ?>
 
-	                <?php do_action( 'youzer_account_before_form'); ?>
+		    <?php do_action('youzer_profile_settings'); ?>
 
-	                <?php do_action( 'youzer_profile_settings' ); ?>
+		    <?php do_action('youzer_account_after_form'); ?>
 
-	                <?php do_action( 'youzer_account_after_form' ); ?>
+		</div>
 
-				</div>
+	    </div>
 
-			</div>
 
-			<?php
-			//if ($is_avatar_page) {
-				$Youzer_Profile = new Youzer_Profile();
-			?>
-				<div class="yz-sidebar-column grid-column yz-profile-sidebar yz-right-sidebar"><?php $Youzer_Profile->sidebar_widgets( 'right', yz_option( 'yz_profile_sidebar_widgets', array(
-						'login'           => 'visible',
-								'user_balance'    => 'visible',
-								'user_badges'     => 'visible',
-								'about_me'        => 'visible',
-								'wall_media'      => 'visible',
-								'social_networks' => 'visible',
-								'friends'         => 'visible',
-								'flickr'          => 'visible',
-								'groups'          => 'visible',
-								'recent_posts'    => 'visible',
-								'user_tags'       => 'visible',
-								'email'           => 'visible',
-								'address'         => 'visible',
-								'website'         => 'visible',
-								'phone'           => 'visible'
-						) ) ); ?></div>
-			<?php //} ?>
+	</main>
 
-		</main>
+	<?php do_action('youzer_account_footer'); ?>
 
-		<?php do_action( 'youzer_account_footer'); ?>
-
-	</div>
+    </div>
 
 </div>
