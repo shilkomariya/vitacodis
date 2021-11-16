755f4df7fe9eb8e84ad0695cdb1bce5a430e57ff
shilkomariya <shilkomariya@gmail.com>
2021-11-16 11:39:51 +0300

    11-40

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -77,4 +77,44 @@
 	$('.register-wrp').show('slow');
     });
 
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.site-header').outerHeight();
+
+    $(window).scroll(function (event) {
+	didScroll = true;
+    });
+
+    setInterval(function () {
+	if (didScroll) {
+	    hasScrolled();
+	    didScroll = false;
+	}
+    }, 250);
+
+    function hasScrolled() {
+	var st = $(this).scrollTop();
+
+	if (!st) {
+	    $('.site-header').addClass('nav-top');
+	} else {
+	    $('.site-header').removeClass('nav-top');
+	}
+
+	if (Math.abs(lastScrollTop - st) <= delta) {
+	    return;
+	}
+
+	if (st > lastScrollTop && st > navbarHeight) {
+	    $('.site-header').removeClass('nav-down').addClass('nav-up');
+	} else {
+	    if (st + $(window).height() < $(document).height()) {
+		$('.site-header').removeClass('nav-up').addClass('nav-down');
+	    }
+	}
+
+	lastScrollTop = st;
+    }
+
 }(jQuery);
