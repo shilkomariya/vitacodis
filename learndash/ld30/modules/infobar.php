a8cacee3463bd59e9aca15549707abec9f4f01ca
shilkomariya <shilkomariya@gmail.com>
2021-11-18 13:58:12 +0300

    13-58

---
+++ learndash/ld30/modules/infobar.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * LearnDash LD30 Displays an informational bar
+ *
+ * Is contextulaized by passing in a $context variable that indicates post type
+ *
+ * $course_status : Course Status
+ *
+ * $user_id       : Current User ID
+ * $context       : course, lesson, topic, quiz, etc...
+ * $logged_in     : User is logged in
+ * $current_user  : (object) Currently logged in user object
+ *
+ * @since 3.0.0
+ *
+ * @package LearnDash\Templates\LD30\Modules
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
