082c260160384011e1ffb5c2795f1d95fae15257
shilkomariya <shilkomariya@gmail.com>
2021-11-18 15:05:56 +0300

    15-00

--- learndash/ld30/modules/course-steps.php
+++ learndash/ld30/modules/course-steps.php
@@ -184,7 +184,6 @@ if (!empty($parent_lesson_id)) {
      */
     do_action('learndash-' . $context . '-course-steps-before', get_post_type(), $course_id, $user_id);
     $learndash_current_post_type = get_post_type();
-    var_dump($learndash_current_post_type);
     ?>
     <?php if ($learndash_current_post_type === "sfwd-topic") { ?>
         <div class="ld-content-action content-title">
