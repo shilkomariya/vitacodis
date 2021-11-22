937f07f66ad47783885f9084d773af6a567e266a
shilkomariya <shilkomariya@gmail.com>
2021-11-22 16:20:09 +0300

    16-20

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -12,7 +12,6 @@ defined('ABSPATH') || exit;
     <a href="<?php the_permalink() ?>"><?php echo get_the_post_thumbnail($post->ID, 'medium-crop', array("class" => "mb-1")); ?></a>
     <div class="text mb-1">
 	<h3 class="h5"><?php the_title() ?></h3>
-	<p class="post-meta"><?php echo get_the_author(); ?><span class="date"><?php echo get_the_date() ?></span></p>
 	<div class="excerpt">
 	    <?php the_excerpt(); ?>
 	</div>
